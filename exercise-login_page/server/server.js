c0f9a98d74e7dccada3498f6fd1af0fee0db27e8
paulo-xavier <px568661@gmail.com>
2024-10-23 21:46:48 -0300

    :lipstick: Applying encryption

--- exercise-login_page/server/server.js
+++ exercise-login_page/server/server.js
@@ -1,8 +1,9 @@
-const express = require('express'); 
+const express = require('express');
 const bodyParser = require('body-parser');
 const mysql2 = require('mysql2');
-const app = express(); 
+const app = express();
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 
 // app.use(cors({
 //     origin: 'http://127.0.0.1:5500'
@@ -11,28 +12,57 @@ const cors = require('cors');
 app.use(cors());
 
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
+
+const saltRounds = 10;
+
+
 
 
 const db = mysql2.createConnection({
-    host: "localhost", 
-    user: "root", 
-    password: "", 
+    host: "localhost",
+    user: "root",
+    password: "",
     database: "login_system"
-}); 
+});
 
 
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    db.query('SELECT * FROM users WHERE user_name = ? AND user_password = ?', [username, password], (err, results) => {
+    // console.log(username);
+    // console.log(password);
+
+    db.query('SELECT * FROM users WHERE user_username = ?', [username], (err, results) => {
+        
         if (err) throw err;
-        if (results.length > 0) {
-            res.sendStatus(200); 
-        } else {
-            res.status(401).send('Well succed');
+        
+        if (results.length === 0) {
+            return res.status(400).send('Invalid username or password');
         }
+
+
+        const storedHash = results[0].user_password;
+        
+        console.log(storedHash)
+
+        bcrypt.compare(password, storedHash, (err, isMatch) => {
+
+            // console.log('aqui')
+
+            if (err) throw err;
+
+            if (isMatch) {
+                console.log("Is matched");
+                return res.status(201).send('Finished');
+            
+            } else {
+                
+                return res.status(400).send('Invalid password')
+            }
+
+        })
 
     })
 })
@@ -40,31 +70,49 @@ app.post('/login', (req, res) => {
 
 app.post('/register', (req, res) => {
 
-    const { name, email, username, password} = req.body;
+    const { name, email, username, password } = req.body;
 
-        db.query('INSERT INTO users (user_name, user_email, user_username, user_password) VALUES (?, ?, ?, ?)', [name, email, username, password], (err, result) => {
-            if (err) throw err;
-            res.sendStatus(201); // Usuário registrado com sucesso
-        });
+    // db.query('INSERT INTO users (user_name, user_email, user_username, user_password) VALUES (?, ?, ?, ?)', [name, email, username, password], (err, result) => {
+    //     if (err) throw err;
+    //     res.sendStatus(201); 
+    // });
+
+    console.log(password);
+
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+
+    if (err) throw err;
+
+    db.query('INSERT INTO users (user_name, user_email, user_username, user_password) VALUES (?, ?, ?, ?)', [name, email, username, hash], (err, result) => {
+        if (err) throw err;
+        
+        res.sendStatus(201);
     });
 
-   
+
+    });
+
+
+});
+
+
 
 app.get('/registers', (req, res) => {
     db.query('SELECT * FROM users', (error, results) => {
-      if (error) {
-        res.status(500).send('Erro ao obter usuários.');
-        return;
-      }
-      res.json(results);
+        if (error) {
+            res.status(500).send('Erro ao obter usuários.');
+            return;
+        }
+        res.json(results);
     });
-  });    
+});
 
-  
+
 
 
 app.put('/users/:id', (req, res) => {
-    
+
     const { id } = req.params;
     const { username, password } = req.body;
 
